363e7efe1b1fd01b4ef1198a420724739f5c8762
ethanforrester <egforrester@mail.fhsu.edu>
2024-04-24 23:04:10 -0400

    HTML templates added

--- golfscore_tracker/scoretracker/models.py
+++ golfscore_tracker/scoretracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Score(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    course_name = models.CharField(max_length=100)
+    scores = models.JSONField()  # Stores scores for each hole
+
+    def __str__(self):
+        return f"{self.user.username} on {self.date} at {self.course_name}"
--- golfscore_tracker/scoretracker/views.py
+++ golfscore_tracker/scoretracker/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Score
+from .forms import ScoreForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home(request):
+    scores = Score.objects.all()
+    return render(request, 'scoretracker/home.html', {'scores': scores})
+
+@login_required
+def add_score(request):
+    if request.method == 'POST':
+        form = ScoreForm(request.POST)
+        if form.is_valid():
+            score = form.save(commit=False)
+            score.user = request.user
+            score.save()
+            return redirect('home')
+    else:
+        form = ScoreForm()
+    return render(request, 'scoretracker/add_score.html', {'form': form})
